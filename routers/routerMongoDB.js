7d212bbaeae630c83570396bee743efa28150ad4
Joaquin del VAL <delvaljoaquin@gmail.com>
2022-07-01 02:03:07 -0300

    mongoDB crud completo

--- routers/routerMongoDB.js
+++ routers/routerMongoDB.js
@@ -6,6 +6,9 @@ const storage = multer({destinantion: "/upload"});
 
 let mongoDBContainer = require("../clases/mongoDBClass.js");
 
+/**
+ * Metodo para obtener Todos los Usuarios y mostarrlos porpantalla
+ */
 routerMongoDB.get("/mongoDB", (req, res, next) => {
     const mostrarProductos = async () => {
         const productos = new mongoDBContainer("ecommerce", "usuarios");
@@ -15,45 +18,28 @@ routerMongoDB.get("/mongoDB", (req, res, next) => {
     mostrarProductos();
 });
 
+/**
+ * Obtengo un usuario segun ID y mostrarlo por pantalla
+ */
 routerMongoDB.get("/mongodb/:id", (req, res, next) => {
     let id = (req.params.id);
     const mostrarProdID = async () => {
-    const productos = new mongoDBContainer("ecommerce", "usuarios");
-        const mostrarID = await productos.getById(id);
+    const usuarios = new mongoDBContainer("ecommerce", "usuarios");
+        const mostrarID = await usuarios.getById(id);
         res.send(mostrarID);
     };
     mostrarProdID();
 });
 
-const productoSubido = storage.fields([
-    {
-        nombre: "title",
-        price: "price",
-        thumbnail: "thumbnail",
-        description: "descripcion",
-        codigo: null,
-        stock: null
-    },
-]);
+/**
+ * Subo un producto Nuevo
+ */
 
-routerMongoDB.post("/mongodb", productoSubido, async (req, res, next) => {
+routerMongoDB.post("/mongodb", async (req, res, next) => {
     const subirProduct = async () => {
         const productos = new mongoDBContainer("ecommerce", "usuarios");
-        if (
-            req.body.title === "" ||
-            req.body.price === "" ||
-            req.body.thumbnail === "" ||
-            req.body.description === "" ||
-            req.body.codigo === "" ||
-            req.body.stock === ""
-        ) {
-            return res.status(400).send({
-                error: "No se pudo cargar el producto. Complete los campos vacios.",
-            });
-        } else {
             await productos.metodoSave(req.body);
             return res.send(req.body);
-        }
         next();
     };
 subirProduct();
