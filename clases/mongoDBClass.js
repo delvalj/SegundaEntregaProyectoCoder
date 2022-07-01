15b607bfcc3842053efd4ede1aef059ce6deba00
Joaquin del VAL <delvaljoaquin@gmail.com>
2022-06-30 23:00:20 -0300

    metodo post en mongodb

--- clases/mongoDBClass.js
+++ clases/mongoDBClass.js
@@ -1,22 +1,42 @@
-const {MongoClient} = require("mongodb");
+const { MongoClient } = require("mongodb");
 
-const mongo = new MongoClient("mongodb+srv://delvalj:Joaquin712@cluster0.vznga.mongodb.net/?retryWrites=true&w=majority");
+const mongo = new MongoClient(
+  "mongodb+srv://delvalj:Joaquin712@cluster0.vznga.mongodb.net/?retryWrites=true&w=majority"
+);
 mongo.connect();
 
 module.exports = class ContenedorMongoDB {
-    constructor(nombreTabla, nombreCollection) {
-      this.nombreTabla = nombreTabla;
-      this.nombreCollection = nombreCollection;
-    }
-
-async metodoSave() {
-    // await mongo.db("ecommerce").collection("usuarios").insertOne({nombre: 'Federico', apellido: 'Garcia Lorca', dni: 17282712});
-    await mongo.db(this.nombreTabla).collection(this.nombreCollection).insertOne({nombre: 'Joaquin', apellido: 'del Val', dni: 17282712});
-}
-
-async getAll() {
-    const resultado = (await mongo.db(this.nombreTabla).collection(this.nombreCollection).find().toArray());
-    console.log(resultado)
+  constructor(nombreTabla, nombreCollection) {
+    this.nombreTabla = nombreTabla;
+    this.nombreCollection = nombreCollection;
   }
-}
 
+  async metodoSave(producto) {
+    await mongo
+      .db(this.nombreTabla)
+      .collection(this.nombreCollection)
+      .insertOne({
+        nombre: producto.nombre,
+        apellido: producto.apellido,
+        dni: producto.dni,
+      });
+  }
+
+  async getAll() {
+    const resultado = await mongo
+      .db(this.nombreTabla)
+      .collection(this.nombreCollection)
+      .find()
+      .toArray();
+    console.log(resultado);
+  }
+
+  // async getById(id) {
+  //   try {
+  //     const resultado = (await mongo.db(this.nombreTabla).collection(this.nombreCollection).find().toArray());
+  //     return resultado.find((p) => p.id === id);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+};
--- routers/routerMongoDB.js
+++ routers/routerMongoDB.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const {Router} = express;
 const routerMongoDB = Router();
+const multer = require("multer");
+const storage = multer({destinantion: "/upload"});
 
 let mongoDBContainer = require("../clases/mongoDBClass.js");
 
@@ -13,49 +15,49 @@ routerMongoDB.get("/mongoDB", (req, res, next) => {
     mostrarProductos();
 });
 
-// routerMongoDB.get("/carrito/:id", (req, res, next) => {
-//     let id = parseInt(req.params.id);
-//     const mostrarProdID = async () => {
-//         const productos = new carritoContainer("carrito.txt");
-//         const mostrarID = await productos.getById(id);
-//         res.send(mostrarID);
-//     };
-//     mostrarProdID();
-// });
+routerMongoDB.get("/mongodb/:id", (req, res, next) => {
+    let id = parseInt(req.params.id);
+const mostrarProdID = async () => {
+    const productos = new mongoDBContainer("ecommerce", "usuarios");
+        const mostrarID = await productos.getById(id);
+        res.send(mostrarID);
+    };
+    mostrarProdID();
+});
 
-// const productoSubido = storage.fields([
-//     {
-//         title: "title",
-//         price: "price",
-//         thumbnail: "thumbnail",
-//         description: "descripcion",
-//         codigo: null,
-//         stock: null
-//     },
-// ]);
+const productoSubido = storage.fields([
+    {
+        nombre: "title",
+        price: "price",
+        thumbnail: "thumbnail",
+        description: "descripcion",
+        codigo: null,
+        stock: null
+    },
+]);
 
-// routerMongoDB.post("/carrito", productoSubido, async (req, res, next) => {
-//     const subirProduct = async () => {
-//         let produc = new carritoContainer("carrito.txt");
-//         if (
-//             req.body.title === "" ||
-//             req.body.price === "" ||
-//             req.body.thumbnail === "" ||
-//             req.body.description === "" ||
-//             req.body.codigo === "" ||
-//             req.body.stock === ""
-//         ) {
-//             return res.status(400).send({
-//                 error: "No se pudo cargar el producto. Complete los campos vacios.",
-//             });
-//         } else {
-//             await produc.metodoSave(req.body);
-//             return res.send(req.body);
-//         }
-//         next();
-//     };
-// subirProduct();
-// });
+routerMongoDB.post("/mongodb", productoSubido, async (req, res, next) => {
+    const subirProduct = async () => {
+        const productos = new mongoDBContainer("ecommerce", "usuarios");
+        if (
+            req.body.title === "" ||
+            req.body.price === "" ||
+            req.body.thumbnail === "" ||
+            req.body.description === "" ||
+            req.body.codigo === "" ||
+            req.body.stock === ""
+        ) {
+            return res.status(400).send({
+                error: "No se pudo cargar el producto. Complete los campos vacios.",
+            });
+        } else {
+            await productos.metodoSave(req.body);
+            return res.send(req.body);
+        }
+        next();
+    };
+subirProduct();
+});
 
 // routerCarrito.delete("/carrito/:id", (req, res) => {
 //     let id = parseInt(req.params.id);
